99911c14bd6906c9b3898ec4c56017f52ae01206
Hiteshwar Singh <77894106+hiteshwar1@users.noreply.github.com>
2026-05-18 21:07:54 +0530

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    int helper(string s, int i, vector<int>& dp) {
+        if (i == s.size()) return 1;
+        if (s[i] == '0') return 0;
+        if (dp[i] != -1) return dp[i];
+        int one = helper(s, i + 1, dp);
+        int two = 0;
+        if ((i < s.size() - 1) && ((s[i] == '1') || (s[i] == '2' && s[i + 1] <= '6')))
+            two = helper(s, i + 2, dp);
+        return dp[i] = one + two;
+    }
+
+    int numDecodings(string s) {
+        vector<int> dp(s.size(), -1);
+        return helper(s, 0, dp);
+    }
+};
