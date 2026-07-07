cb1b1f5d0c509fffb8a07a426acd8327ef26f39b
Hiteshwar Singh <77894106+hiteshwar1@users.noreply.github.com>
2026-07-07 22:42:43 +0530

    Add: palindromic-substrings - submission-1

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    void helper(string s, int i, int j, int& res) {
+        while (i >= 0 && j < s.size() && s[i] == s[j]) {
+            res++;
+            i--;
+            j++;
+        }
+    }
+    int countSubstrings(string s) {
+        int res = 0;
+        for (int i = 0; i < s.size(); i++) {
+            helper(s, i, i, res);
+            helper(s, i, i + 1, res);
+        }
+        return res;
+    }
+};
